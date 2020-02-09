f2a75186ca8fbc71cfba121f1422b2088a71bce6
christinamcmahon <christina.mcmahon1@gmail.com>
2020-02-09 11:55:41 -0800

    Done.

--- rendering/app/controllers/responses_controller.rb
+++ rendering/app/controllers/responses_controller.rb
@@ -1,14 +1,14 @@
 class ResponsesController < ApplicationController
-  require 'csv'
+  require "csv"
 
   # for each of the controller actions,
   # render the data according to the name of the action
   def plain
-    data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
+    render plain: data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
   end
 
   def html
-    data = "<html>
+    render html: data = "<html>
               <body>
                 <q>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</q>
                 <cite>Cookie Monster</cite>
@@ -17,25 +17,26 @@ class ResponsesController < ApplicationController
   end
 
   def json
-    data = {
+    render json: data = {
       author: "Cookie Monster",
-      quote:"Early bird gets the worm. But cookie taste better than worm. So me sleep in.",
+      quote: "Early bird gets the worm. But cookie taste better than worm. So me sleep in.",
     }
   end
 
   def xml
-    data = "<quote>
+    render xml: data = "<quote>
     <author>Cookie Monster</author>
     <text>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</text>
     </quote>"
   end
 
   def csv
-    data =  CSV.generate do |csv|
+    @data = CSV.generate do |csv|
       csv << ["author", "name"]
       csv << ["Cookie Monster", "Early bird gets the worm. But cookie taste better than worm. So me sleep in."]
     end
 
+    send_data @data
     # you'll need to use send_data instead of render
     # if you want the file to have a particular filename, pass the filename: parameter
   end
@@ -43,6 +44,7 @@ class ResponsesController < ApplicationController
   def png
     filename = "public/data.png"
 
+    send_file filename, displosition: :inline
     # you'll need to use send_file instead of render
     # if you want the image to show inline, use disposition: :inline
   end
